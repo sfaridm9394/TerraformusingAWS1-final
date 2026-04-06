71c98458f8af7e3c1c91e610b5bb2be389874a16
Farid <shahussainfaridm@gmail.com>
2026-04-06 11:50:34 +0530

    user data

---
+++ userdata.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+yum update -y
+
+# Install Python3 and pip
+yum install -y python3 python3-pip
+
+# Install Flask
+pip3 install flask
+
+# Create simple HA web app
+cat << 'EOF' > /home/ec2-user/app1.py
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "✅ Hello from EC2 Instance 1 - ALB Test Successful!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
+EOF
+
+# Run the Flask app in the background
+nohup python3 /home/ec2-user/app1.py &
